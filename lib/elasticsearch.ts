34368e4bc32d3d0288106dfb694801de6cc2cea0
pankajkandpal <kandpalpankaj34@gmail.com>
2025-03-01 11:54:45 +0530

    fix

--- lib/elasticsearch.ts
+++ lib/elasticsearch.ts
@@ -22,7 +22,4 @@ export const elasticClient = new Client({
           password: process.env.ELASTICSEARCH_PROD_PASSWORD ?? "changeme",
         }
       : undefined,
-  headers: {
-    "Content-Type": "application/json",
-  },
 });
